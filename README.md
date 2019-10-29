3d1f2a31779525d73e2f6b1ab0f66f1271097b2c
Tom Lee <contact@tomlee.com.au>
2019-10-29 13:56:47 +1000

    Arrange inputs and outputs to match ATB trajectory repo structure

--- README.md
+++ README.md
@@ -5,9 +5,15 @@ a molecular dynamics simulation of hen's egg lysozyme protein using the
 Amber18 simulation software on the University of Queensland Research Computing 
 Centre (RCC) Wiener GPU cluster.
 
-The information is up to date as of 9/5/2019.
+The information is up to date as of 29/10/2019.
 
 To run a simulation, copy these files to your scratch space directory,
 cd to that directory, and run `sbatch submit.sh`.
 
+The submission script will automatically arrange files in the structure required but
+the [ATB Trajectory repository](www.molecular-dynamics.atb.uq.edu.au). You can change
+the output directory by modifying the `working` variable in the submission script.
+
+The job will self-resubmit in order to continue the run. To disable this, remove the line
+`sbatch submit.sh` near the end of the `submit.sh` script.
 
--- submit.sh
+++ submit.sh
@@ -6,15 +6,109 @@
 #####SBATCH --gres=gpu:1 #use this for one gpu on any node
 #####SBATCH --gres=gpu:tesla:2 #use this for slower nodes with 2 gpus per node
 #SBATCH --mem-per-cpu=1G
-#SBATCH --error=error_messages.log
-#SBATCH --output=standard_output.log
+#SBATCH --error=stderr.txt
+#SBATCH --output=stdout.txt
+#SBATCH --open-mode=append
 
+date
+
+# Abort on error
+set -o errexit
+set -o pipefail
+set -o nounset
+
+############## CHECK THESE!! #####
+#
+#
+working=working
+name=example
+topology=topology.prmtop
+initial_coordinates=coordinates.mdcrd #initial coordinates for simulation
+control_template=control.mdin #
+numgpu=4
+#
+#
+##################################
+
+# If first run
+if ! [ -d $working ]
+then
+    #create subdirectories
+    for directory in control stderr stdout trajectory velocity input-coordinates energy topology log 
+    do
+        mkdir -p $working/$directory
+    done
+    cp $initial_coordinates $working/input-coordinates/${name}_input-coordinates_1.$(awk -F . '{print $NF}' <<< $initial_coordinates)
+    cp $topology $working/topology/$(basename ${name}_topology_1.prmtop)
+fi
+
+set +o pipefail #briefly stop aborting on error
+# if any log files exists
+if ls $working/log/${name}_log_*.log
+then
+    # get name of last log file
+    last_log=$(ls -v $working/log/${name}_log_*.log | tail -n1)
+    # truncate directory and extension
+    last_log_base=$(basename -s .log $last_log)
+    # get the number of the last log file
+    prev_i=$(awk -F _ '{print $NF}'  <<< $last_log_base)
+else # if no log files found
+    prev_i=0
+fi
+set +o pipefail
+
+i=$(bc <<< "${prev_i}+1")
+next_i=$(bc <<< "$i+1")
+
+# Redirect standard error and standard output
+stdout=$working/stdout/${name}_stdout_$i.txt
+stderr=$working/stderr/${name}_stderr_$i.txt
+exec > $stdout  2> $stderr
+
+date
+
+# Abort if the last log file didn't finish cleanly
+prev_log=$working/log/${name}_log_$prev_i.log
+if [ -f $prev_log]
+then
+    grep '|  Master Total wall time:' $prev_log
+fi
+
+# Load required modules
 module load gnu7 mvapich2 pmix cuda/9.2.148.1 amber/18.2
 
-# -n4 = 4 cpu. Should be the same as the number of GPUs available, not the number of CPUs available.
-srun -n4 --mpi=pmi2 pmemd.cuda.MPI -O \
-    -i   input.mdin           -c coordinates.mdcrd     -p topology.prmtop  \
-    -r final_coordinates.nc   -x trajectory.nc         -e energy.mden     \
-    -inf mdinfo               -o logfile.log
+# Input and output files
+control=$working/control/${name}_control_$i.mdin
+coords=$(ls $working/input-coordinates/${name}_input-coordinates_$i.*)
+final=$working/input-coordinates/${name}_input-coordinates_$next_i.nc
+trajectory=$working/trajectory/${name}_trajectory_$i.nc
+velocity=$working/velocity/${name}_velocity_$i.nc
+energy=$working/energy/${name}_energy_$i.nc
+log=$working/log/${name}_log_$i.log
+topology=$working/topology/${name}_topology_1.prmtop
+
+cp $control_template $control
+
+echo Starting run $i
+
+# Run molecular dynamics
+srun -n$numgpu --mpi=pmi2 \
+    pmemd.cuda.MPI -O \
+    -i $control \
+    -c $coords \
+    -p $topology  \
+    -r $final \
+    -x $trajectory \
+    -v $velocity \
+    -e $energy   \
+    -inf mdinfo \
+    -o $log
+
+echo Finished run $i
+date
+
+echo Submitting continuation run
+sbatch submit.sh
 
 
+exit
